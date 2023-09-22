82f0bad61fe51a4a836dbb661d911070144f9313
Wei Guo <851153978@qq.com>
2023-09-22 21:54:15 +0800

    foc: Add svpwm inv clark script

--- 2.foc/foc_svpwm.py
+++ 2.foc/foc_svpwm.py
@@ -1,136 +1,122 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def svpwm(sector, a, b, c):
-    if sector == 1:
-        x = a
-        y = b
-        z = -(x + y)
+alphad = np.arange(0, 360, 1)
+alpha = np.deg2rad(alphad)
 
-        da = x + y + 0.5 * z
-        db = y + 0.5 * z
-        dc = 0.5 * z
-    elif sector == 2:
-        x = -c
-        y = -a
-        z = -(x + y)
-
-        da = x + 0.5 * z
-        db = x + y + 0.5 * z
-        dc = 0.5 * z
-    elif sector == 3:
-        x = b
-        y = c
-        z = -(x + y)
-
-        da = 0.5 * z
-        db = x + y + 0.5 * z
-        dc = y + 0.5 * z
-    elif sector == 4:
-        x = -a
-        y = -b
-        z = -(x + y)
-
-        da = 0.5 * z
-        db = x + 0.5 * z
-        dc = x + y + 0.5 * z
-    elif sector == 5:
-        x = c
-        y = a
-        z = -(x + y)
-
-        da = y + 0.5 * z
-        db = 0.5 * z
-        dc = x + y + 0.5 * z
-    elif sector == 6:
-        x = -b
-        y = -c
-        z = -(x + y)
-
-        da = x + y + 0.5 * z
-        db = 0.5 * z
-        dc = x + 0.5 * z
-    return da * 2, db * 2, dc *2
-
-
-def calc(sector, angle):
-    alpha = np.deg2rad(angle)
-
-    va = np.cos(alpha)
-    vb = np.sin(alpha)
-
-    # amplitude assumed sqrt(3) / 2 (maximum)
-    a = va - (1 / np.sqrt(3) * vb)
-    b = 2.0 / np.sqrt(3) * vb
-    c = -(a + b)
-
-    return svpwm(sector, a, b, c)
-
-def calc_offset(sector, angle):
-    alpha = np.deg2rad(angle)
-
-    va = np.cos(alpha)
-    vb = np.sin(alpha)
-
-    a = (2 / np.sqrt(3)) * va
-    b = vb - (1 / np.sqrt(3)) * va
-    c = -(a + b)
-
-    return svpwm(sector, a, b, c)
-
-fig, (ax, ax2) = plt.subplots(1, 2)
+fig, ax = plt.subplots()
 
 ax.set_xlabel("State space vector angle (deg)")
 ax.set_ylabel("Duty cycle")
 
-alpha = np.array([])
-alpha_offset = np.array([])
 da = np.array([])
 db = np.array([])
 dc = np.array([])
 
-ta = np.array([])
-tb = np.array([])
-tc = np.array([])
+def get_sector(a, b, c):
+    sector = 0
+    if (c < 0):
+        if (a < 0):
+            sector = 2
+        else:
+            if (b < 0):
+                sector = 6
+            else:
+                sector = 1
+    else:
+        if (a < 0):
+            if (b <= 0):
+                sector = 4
+            else:
+                sector = 3
+        else:
+            sector = 5
 
-for sector in range(1, 7):
-    alpha_ = np.arange(60 * (sector - 1), 60 * sector, 1)
-    alpha_offset_ = np.arange(60 * (sector - 1) + 30, 60 * sector + 30, 1)
-    da_, db_, dc_ = calc(sector, alpha_)
-    ta_, tb_, tc_ = calc_offset(sector, alpha_offset_)
+    return sector
 
-    alpha = np.append(alpha, alpha_)
-    alpha_offset = np.append(alpha_offset, alpha_offset_)
-    da = np.append(da, da_)
-    db = np.append(db, db_)
-    dc = np.append(dc, dc_)
-    ta = np.append(ta, ta_)
-    tb = np.append(tb, tb_)
-    tc = np.append(tc, tc_)
+for rad in alpha:
+    va = np.cos(rad)
+    vb = np.sin(rad)
 
-ax.plot(alpha, da, color="r", label=r"$d_a$")
-ax.plot(alpha, db, color="g", label=r"$d_b$")
-ax.plot(alpha, dc, color="b", label=r"$d_c$")
+    a = va
+    b = -0.5 * va + 0.8660254039 * vb
+    c = -(a + b)
 
-ax2.plot(alpha_offset, ta, color="r", label=r"$d_a$")
-ax2.plot(alpha_offset, tb, color="g", label=r"$d_b$")
-ax2.plot(alpha_offset, tc, color="b", label=r"$d_c$")
+    k = (2 / np.sqrt(3))
+    a = a * k
+    b = b * k
+    c = c * k
+    sector = get_sector(a, b, c)
+
+    if (sector == 1):
+        x = a;
+        y = b;
+        z = 1 - (x+y)
+
+        a = x + y + z * 0.5
+        b = y + z * 0.5
+        c = z * 0.5
+    elif(sector == 2):
+        x = -c;
+        y = -a;
+        z = 1 - (x+y)
+
+        a = x + z * 0.5
+        b = x + y + z * 0.5
+        c = z * 0.5
+    elif(sector == 3):
+        x = b;
+        y = c;
+        z = 1 - (x+y)
+
+        a = z * 0.5
+        b = x + y + z * 0.5
+        c = y + z * 0.5
+    elif(sector == 4):
+        x = -a;
+        y = -b;
+        z = 1 - (x+y)
+
+        a = z * 0.5
+        b = x + z * 0.5
+        c = x + y + z * 0.5
+    elif(sector == 5):
+        x = c;
+        y = a;
+        z = 1 - (x+y)
+
+        a = y + z * 0.5
+        b = z * 0.5
+        c = x + y + z * 0.5
+    elif(sector == 6):
+        x = -b;
+        y = -c;
+        z = 1 - (x+y)
+
+        a = x + y + z * 0.5
+        b = z * 0.5
+        c = x + z * 0.5
+    else:
+        print("Error")
+
+    da = np.append(da, a)
+    db = np.append(db, b)
+    dc = np.append(dc, c)
+
+ax.plot(alphad, da, color="r", label=r"$d_a$")
+ax.plot(alphad, db, color="g", label=r"$d_b$")
+ax.plot(alphad, dc, color="b", label=r"$d_c$")
 
 ax.axvline(0, ls="--")
 ax.axvline(60, ls="--")
 ax.axvline(120, ls="--")
+ax.axvline(180, ls="--")
 ax.axvline(240, ls="--")
 ax.axvline(300, ls="--")
 ax.axvline(360, ls="--")
 
-ax2.axvline(0, ls="--")
-ax2.axvline(60, ls="--")
-ax2.axvline(120, ls="--")
-ax2.axvline(240, ls="--")
-ax2.axvline(300, ls="--")
-ax2.axvline(360, ls="--")
-
-
-ax.legend(loc="upper right")
+ax.axhline(0, ls="--")
+ax.axhline(1, ls="--")
 
 plt.show()
+
